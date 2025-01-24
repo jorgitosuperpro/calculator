efa32d755444b6b7d1244736918588076c5ee957
Jorge <srcoke98@gmail.com>
2025-01-24 18:18:39 +0100

    Improved CSS and Display

--- javascript.js
+++ javascript.js
@@ -21,3 +21,17 @@ function operate(operator, a, b) {
     if (operator == "/") return divide(a,b)
 }
 
+function clearDisplay() {
+    document.getElementById("display").innerText = "";
+}
+
+function printOperand(operand) {
+    document.getElementById("#display").innerText = operand;
+}
+
+let clear = document.querySelector("#clear")
+clear.addEventListener("click", clearDisplay)
+let buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+    button.addEventListener("click", printOperand(button.innerText))
+});
