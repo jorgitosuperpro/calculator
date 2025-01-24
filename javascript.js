e5086767c8c8136f58e0802b7ac5d82735d13e0a
Jorge <srcoke98@gmail.com>
2025-01-24 01:27:06 +0100

    Added basic functionality and html was created

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function add(a,b) {
+    return a+b
+}
+
+function substract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+
+function operate(operator, a, b) {
+    if (operator == "+") return add(a,b)
+    if (operator == "-") return substract(a,b)
+    if (operator == "*") return multiply(a,b)
+    if (operator == "/") return divide(a,b)
+}
+
