263c3f2fd1ad50140943cba72809b96c59ab711b
Jorge <srcoke98@gmail.com>
2025-01-27 19:49:08 +0100

    Improved functionality

--- javascript.js
+++ javascript.js
@@ -28,18 +28,28 @@ function operate(operator, a, b) {
 //Erase display from screen
 function clearDisplay() {
     display.innerText = "0"
+    
+}
+function clearsecondDisplay () {
+    second_display.innerText = ""
 }
 //I create variables and DOM
 let x = ""
 let y = ""
 let operator = ""
 let clear = document.querySelector("#clear")
-let display = document.getElementById("display")
-clear.addEventListener("click", clearDisplay)
+let display = document.getElementById("third-display")
+let second_display = document.querySelector("#second-display")
+clear.addEventListener("click", function() {
+    clearDisplay()
+    clearsecondDisplay()
+})
 let digits = document.querySelectorAll("#digits") 
 let operators = document.querySelectorAll("#operator")
 let integer = document.querySelector("#integer")
 let decimal = document.querySelector("#decimal")
+let backspace = document.querySelector("#backspace")
+let percent = document.querySelector("#percent")
 //For each digit button and eventListener is added and the digit is displayed.
 for (let i=0; i<digits.length; i++) {
     digits[i].addEventListener("click", function() {
@@ -56,6 +66,8 @@ for (let i=0; i<operators.length; i++) {
     operators[i].addEventListener("click", function() {
         x = display.innerHTML
         operator = operators[i].innerHTML
+        let buffer = x + "" + operator
+        second_display.innerHTML = buffer
         clearDisplay()
     })
 }   
@@ -71,16 +83,32 @@ integer.addEventListener("click", function() {
 })
 //Add decimal number
 decimal.addEventListener("click", function() {
-    if(display.innerHTML.includes('.')) {
+    if (display.innerHTML.includes('.')) {
         decimal.removeEventListener
     } else {
         display.innerHTML += "."
     }
 })
+//Backspace
+backspace.addEventListener("click", function() {
+    let lengthofdisplay = display.innerHTML.length
+    if (lengthofdisplay ==1) {
+        display.innerHTML = display.innerHTML.substring(0, lengthofdisplay-1)
+        display.innerHTML =  "0"
+    } else {
+        display.innerHTML = display.innerHTML.substring(0, lengthofdisplay-1)
+    }
+})
+//Percent
+percent.addEventListener("click", function() {  
+    let percent_int  =  parseFloat(display.innerHTML)/100
+    display.innerHTML =  percent_int.toString()
+})
 //Evaluate the operation, operate and display value on screen
 let evaluate = document.querySelector("#evaluate")
 evaluate.addEventListener("click", function() {
     y = display.innerHTML
+    second_display.innerHTML += y
     clearDisplay()
     operate(operator, parseFloat(x), parseFloat(y))
 })
